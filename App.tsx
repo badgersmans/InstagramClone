07b00b193738ed1fb228e4f41fde0ac12740a9cd
Shawn Law <shawnlaw@tuta.io>
2023-06-15 13:08:15 +0800

    initial commit

---
+++ App.tsx
@@ -0,0 +1,15 @@
+import { View, Text } from 'react-native'
+import colors from './src/theme/colors'
+import fonts from './src/theme/fonts'
+
+const App = () => {
+
+
+  return (
+    <View style={{flex: 1, justifyContent: 'center', alignItems: 'center'}}>
+      <Text>Akkjnkjnpp</Text>
+    </View>
+  )
+}
+
+export default App
