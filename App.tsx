1ed714501047c6fa504326f2520b0a6738bf779e
Shawn Law <shawnlaw@tuta.io>
2023-06-21 16:59:44 +0800

    5.5 connect to aws amplify

--- App.tsx
+++ App.tsx
@@ -1,4 +1,8 @@
 import Navigation from './src/navigation';
+import { Amplify } from 'aws-amplify';
+import awsExports from './src/aws-exports';
+
+Amplify.configure(awsExports);
 
 const App = () => {
   return (
